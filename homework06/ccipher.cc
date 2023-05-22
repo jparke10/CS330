a69fed7637fbd374c4cbd2d3ec8a5eb14a6147b1
jparke10 <129916202+jparke10@users.noreply.github.com>
2023-05-21 21:17:43 -0700

    latest version for gdb

--- homework06/ccipher.cc
+++ homework06/ccipher.cc
@@ -16,29 +16,30 @@ CCipher::~CCipher() {}
 // -------------------------------------------------------
 
 string CCipher::encrypt(string raw) {
-	// seems safer to work with new string
-	string enc = raw;
-	rotate_string(enc, offset);
-	return enc;
+	// seems safer to work with string copy
+	string ret = raw;
+	rotate_string(ret, offset);
+	return ret;
 }
 
 string CCipher::decrypt(string enc) {
-	string dec = enc;
-	rotate_string(dec, 0 - offset);
-	return dec;
+	string ret = enc;
+	rotate_string(ret, -offset);
+	return ret;
 }
 
 // Rotates the input string in_str by rot positions
 void rotate_string(string& in_str, int rot)
 {
-	for (size_t i = 0; i < in_str.length(); i++) {
+	size_t len = in_str.length();
+	for (size_t i = 0; i < len; i++) {
 		// 5.ii.b
 		if (in_str[i] == ' ')
 			continue;
 		char working_char = LOWER_CASE(in_str[i]);
 		// work through ASCII table, use order of operations and
 		// loop around alphabet with modulo
-		char rotated_char = ((working_char - 'a') + rot) % 26 + 'a';
+		char rotated_char = (working_char - 'a' + rot) % 26 + 'a';
 		// 5.ii.a
 		if (in_str[i] >= 'A' && in_str[i] <= 'Z')
 			in_str[i] = UPPER_CASE(rotated_char);
